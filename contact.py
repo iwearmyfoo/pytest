2ad8931a701ff0790c213eef053160e3f7df859f
ubuntu <iwearmyfoofayka@gmail.com>
2020-07-06 17:05:18 +0530

    contact test

---
+++ contact.py
@@ -0,0 +1,44 @@
+class Contact:
+
+    def __init__(self,  fistname, midname, lastname, nickname, title, company, address, homephone,
+                 mobilephone, workphone, faxphone, firstmail, secondmail, thirdmail, homepage, bday,
+                 bmonth, byear, aday, amonth, ayear, secondaryaddress, additionalphone, notes):
+        self.fistname = fistname
+        self.midname = midname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.faxphone = faxphone
+        self.firstmail = firstmail
+        self.secondmail = secondmail
+        self.thirdmail = thirdmail
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.secondaryaddress = secondaryaddress
+        self.additionalphone = additionalphone
+        self.notes = notes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
